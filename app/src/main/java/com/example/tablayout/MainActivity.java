e18c10a18c1f6dfe0a526eb133179409c1583ee3
root <soumojyotiganguly270@gmail.com>
2020-08-06 12:25:17 +0530

    test tabbed layout

--- app/src/main/java/com/example/tablayout/MainActivity.java
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -15,18 +18,26 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private TabItem tab1, tab2, tab3;
     public PagerAdapter pagerAdapter;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.click_me);
         tabLayout = findViewById(R.id.tab_layout);
         tab1 = findViewById(R.id.tab_1);
         tab2 = findViewById(R.id.tab_2);
         tab3 = findViewById(R.id.tab_3);
-
         viewPager = findViewById(R.id.view_pager);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
+            }
+        });
 
         pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
@@ -35,12 +46,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-                if (tab.getPosition() == 0)
+                if (tab.getPosition() == 0) {
                     pagerAdapter.notifyDataSetChanged();
-                else if (tab.getPosition() == 1)
+                    button.setVisibility(View.VISIBLE);
+                } else if (tab.getPosition() == 1) {
                     pagerAdapter.notifyDataSetChanged();
-                else if (tab.getPosition() == 2)
+                    button.setVisibility(View.GONE);
+                } else if (tab.getPosition() == 2) {
+                    button.setVisibility(View.VISIBLE);
                     pagerAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
--- app/src/main/java/com/example/tablayout/Tab1.java
+++ app/src/main/java/com/example/tablayout/Tab1.java
@@ -7,18 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Tab1#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+
 public class Tab1 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private View view;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -28,14 +28,6 @@ public class Tab1 extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab1.
-     */
     // TODO: Rename and change types and number of parameters
     public static Tab1 newInstance(String param1, String param2) {
         Tab1 fragment = new Tab1();
@@ -52,13 +44,24 @@ public class Tab1 extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        view = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        TextView textView = view.findViewById(R.id.fab1_text_view);
+        textView.setText("FEB 1");
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Click text view",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
